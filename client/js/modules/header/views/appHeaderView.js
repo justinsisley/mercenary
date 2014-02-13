f2117614e5163f97d91664db0438819b447a1819
Justin Sisley <sisley.justin@gmail.com>
2014-02-13 11:25:36 -0800

    Updated app header link behavior

--- client/js/modules/header/views/appHeaderView.js
+++ client/js/modules/header/views/appHeaderView.js
@@ -8,6 +8,16 @@ define([
     Marionette
 ) {
     return Marionette.ItemView.extend({
-        template: 'header/appHeader'
+        template: 'header/appHeader',
+
+        events: {
+            'click a': 'linkHandler'
+        },
+
+        linkHandler: function(e) {
+            e.preventDefault();
+
+            Backbone.history.navigate(e.target.pathname);
+        }
     });
 });
