32fd042d86606dfd7a704e088fa66d1f7b4cd691
Justin Sisley <justin@veradocs.com>
2014-07-31 21:46:57 -0700

    Updated username value in app header

--- client/js/modules/header/controllers/appHeaderController.js
+++ client/js/modules/header/controllers/appHeaderController.js
@@ -3,6 +3,8 @@ var AppHeaderView = require('modules/header/views/appHeaderView');
 
 module.exports = {
     show: function() {
+        console.log(App.vars.user);
+
         var appHeaderView = new AppHeaderView({model: App.vars.user});
 
         App.appLayout.headerRegion.show(appHeaderView);
