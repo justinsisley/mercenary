2d85c4b1b7422fd8771077d90b276a0cd143d683
Justin Sisley <justin@veradocs.com>
2014-09-05 18:07:32 -0700

    Fixed JSHint task error

--- utils/gulp/jshint.js
+++ utils/gulp/jshint.js
@@ -3,7 +3,7 @@ var jshint  = require('gulp-jshint');
 var config  = require('./config');
 
 gulp.task('jshint', function() {
-    return gulp.src([config.jsSrc])
+    return gulp.src(config.jsSrc)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
