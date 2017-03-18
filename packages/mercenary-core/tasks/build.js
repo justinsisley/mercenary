c7f7f8b0b44b0a28cb68f31997a9f4e451c6ffe6
Justin Sisley <manmademammal@gmail.com>
2017-03-17 21:57:40 -0700

    Resolve webpack binary correctly

--- packages/mercenary-core/tasks/build.js
+++ packages/mercenary-core/tasks/build.js
@@ -3,13 +3,13 @@ const cp = require('child_process');
 
 const cwd = process.cwd();
 const configDir = path.join(__dirname, '../config');
-const npmBin = path.join(cwd, './node_modules/.bin');
+const webpack = require.resolve('.bin/webpack');
 
 // Production build
 const build = (config = { silent: false }) => {
   cp.execSync(`
     rm -rf ${cwd}/static &&
-    NODE_ENV=production "${npmBin}/webpack" \
+    NODE_ENV=production "${webpack}" \
       --display-error-details \
       --config \
       "${configDir}/webpack/production.js"
