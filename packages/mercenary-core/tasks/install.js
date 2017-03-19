70ad1912a280b268af01b07207dc10f806be4260
Justin Sisley <manmademammal@gmail.com>
2017-03-18 20:54:12 -0700

    Allow yarn for install task

--- packages/mercenary-core/tasks/install.js
+++ packages/mercenary-core/tasks/install.js
@@ -1,8 +1,25 @@
-const cp = require('child_process');
+const execSync = require('child_process').execSync;
+
+function shouldUseYarn() {
+  try {
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
 
 const install = () => {
-  // Install fresh dependencies
-  cp.execSync('npm install', { stdio: 'inherit' });
+  let command;
+
+  if (shouldUseYarn()) {
+    command = 'yarnpkg';
+  } else {
+    command = 'npm';
+  }
+
+  execSync(command, ['install'], { stdio: 'inherit' });
 };
 
 module.exports = install;
