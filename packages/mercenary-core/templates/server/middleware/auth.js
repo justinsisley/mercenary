5240c70dc0c1f43823e86f103640b786c7a93a0c
Justin Sisley <manmademammal@gmail.com>
2017-05-14 11:04:30 -0700

    v2.18.3

--- packages/mercenary-core/templates/server/middleware/auth.js
+++ packages/mercenary-core/templates/server/middleware/auth.js
@@ -1,26 +1,8 @@
-const jwt = require('tokenpress').fwt;
 const roles = require('../constants/roles');
 
 module.exports = {
-  requireAuth(req, res, next) {
-    const failed = () => res.status(401).json({});
-
-    const token = req.headers.authorization;
-
-    if (!token) {
-      return failed();
-    }
-
-    return jwt.verify(token).then((user) => {
-      req.user = user;
-
-      next();
-    })
-    .catch(failed);
-  },
-
   requireAdmin(req, res, next) {
-    const isAdmin = req.user.role === roles.ADMIN;
+    const isAdmin = req.jwt.role === roles.ADMIN;
 
     if (!isAdmin) {
       return res.status(403).json({});
