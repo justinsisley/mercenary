0fdfa13a9f17a58e4137446585f57c0d8c84f69d
Justin Sisley <sisley.justin@gmail.com>
2017-05-19 23:12:44 -0700

    v2.33.2

--- packages/mercenary-core/config/index.js
+++ packages/mercenary-core/config/index.js
@@ -6,7 +6,7 @@ const cwd = process.cwd();
 const projectConfigPath = path.join(cwd, './config.js');
 const projectConfig = require(projectConfigPath); // eslint-disable-line
 
-// Create difficult-to-guess default credentials for netdata
+// Create fallback credentials for netdata
 const randomInt = (min, max) => Math.floor(Math.random() * (max - (min + 1))) + min;
 const netdataUsername = generatePassword(randomInt(18, 32), false);
 const netdataPassword = generatePassword(randomInt(18, 32), false);
@@ -23,16 +23,15 @@ module.exports = {
   // Length of time to cache static assets in production mode
   maxAge: process.env.CACHE_MAX_AGE || 1000 * 60 * 60 * 24 * 60, // 60 days
 
-  netdata: {
-    // eslint-disable-next-line
-    username: process.env.NETDATA_USERNAME || projectConfig.deploy.netdata.username || netdataUsername,
-    // eslint-disable-next-line
-    password: process.env.NETDATA_PASSWORD || projectConfig.deploy.netdata.password || netdataPassword,
-  },
-
   // Redis configuration for rate limiting
   redis: {
     host: process.env.REDIS_HOST || projectConfig.redis.host,
     port: process.env.REDIS_PORT || projectConfig.redis.port,
+  },
+
+  // Configure access to netdata dashboard
+  netdata: {
+    username: process.env.NETDATA_USERNAME || projectConfig.deploy.netdata.username || netdataUsername,
+    password: process.env.NETDATA_PASSWORD || projectConfig.deploy.netdata.password || netdataPassword,
   },
 };
