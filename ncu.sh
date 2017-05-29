145809b2f0cc480bd1315b25e52d35a916d98e8b
Justin Sisley <sisley.justin@gmail.com>
2017-05-29 10:13:50 -0700

    Added ncu script

---
+++ ncu.sh
@@ -0,0 +1,23 @@
+# Check root project
+ncu;
+
+# Check babel preset
+cd ./packages/babel-preset-mercenary; ncu
+
+# Check eslint config
+cd ../eslint-config-mercenary; ncu
+
+# Check CLI
+cd ../mercenary; ncu
+
+# Check core
+cd ../mercenary-core; ncu
+
+# Check templates
+cd ./templates; ncu
+
+# Check test
+cd ../../mercenary-test; ncu
+
+# Go back to root project directory
+cd ../../
