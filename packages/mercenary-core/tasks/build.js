80fbccfd4ca5c98c13ce65816d181265e79f1487
Justin Sisley <sisley.justin@gmail.com>
2017-06-12 13:03:59 -0700

    v2.51.5

--- packages/mercenary-core/tasks/build.js
+++ packages/mercenary-core/tasks/build.js
@@ -4,12 +4,13 @@ const cp = require('child_process');
 const cwd = process.cwd();
 const configDir = path.join(__dirname, '../config');
 const webpack = require.resolve('.bin/webpack');
+const maxMemory = 449;
 
 // Production build
 const build = (config = { silent: false }) => {
   cp.execSync(`
     rm -rf ${cwd}/public &&
-    NODE_ENV=production node --max_old_space_size=449 "${webpack}" \
+    NODE_ENV=production node --max_old_space_size=${maxMemory} "${webpack}" \
       --display-error-details \
       --config \
       "${configDir}/webpack/production.js"
