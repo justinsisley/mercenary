5cf84060832cae4d3fd3f0aa89add859b2aa5afc
Justin Sisley <sisley.justin@gmail.com>
2017-07-20 21:34:26 -0700

    v2.68.1

--- packages/mercenary-core/tasks/install.js
+++ packages/mercenary-core/tasks/install.js
@@ -1,7 +1,7 @@
 const execSync = require('child_process').execSync;
 
 const install = () => {
-  execSync('rm -rf node_modules package-lock.json && npm install', { stdio: 'inherit' });
+  execSync('rm package-lock.json && npm install', { stdio: 'inherit' });
 };
 
 module.exports = install;
