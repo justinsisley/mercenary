70cefc703ff7e47e281c1d12092731a24546b1a2
Justin Sisley <sisley.justin@gmail.com>
2017-07-25 14:16:34 -0700

    v2.72.1

--- packages/mercenary-core/server/index.js
+++ packages/mercenary-core/server/index.js
@@ -8,26 +8,18 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const proxy = require('proxy-middleware');
 const url = require('url');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
 const getIp = require('ip');
-const chokidar = require('chokidar');
 const basicAuth = require('basic-auth-connect');
-const webpackConfig = require('../config/webpack/development');
 const config = require('../config');
+const devServer = require('./dev');
 
 // Configurable values
 const ENV = config.env;
 const EXPRESS_PORT = config.expressPort;
 const HOSTNAME = config.hostname;
 const WWW = config.www;
-const WEBPACK_DEV_SERVER_PORT = config.webpackDevServerPort;
 const NETDATA_USERNAME = config.netdata.username;
 const NETDATA_PASSWORD = config.netdata.password;
-
-// Dev server hostname
-const devServerDomain = 'http://localhost';
-const devServerHost = `${devServerDomain}:${WEBPACK_DEV_SERVER_PORT}/`;
 
 // Various references to this local server
 const localhost = `http://localhost:${EXPRESS_PORT}`;
@@ -141,58 +133,9 @@ if (fileExists(localServerPath)) {
   }
 }
 
-// Development environment configuration
 if (ENV === 'development') {
-  // Proxy static assets to webpack-dev-server
-  app.use('/', proxy(url.parse(devServerHost)));
-
-  // Create new webpack dev server with hot reloading enabled
-  const webpackDevServer = new WebpackDevServer(webpack(webpackConfig), {
-    publicPath: webpackConfig.output.publicPath,
-    historyApiFallback: true,
-    hot: true,
-    // More minimal logging than the "minimal" setting
-    stats: {
-      assets: false,
-      cached: false,
-      cachedAssets: false,
-      children: false,
-      chunks: false,
-      chunkModules: false,
-      chunkOrigins: false,
-      hash: false,
-      maxModules: 0,
-      modules: false,
-      moduleTrace: false,
-      performance: false,
-      publicPath: false,
-      reasons: false,
-      source: false,
-      timings: false,
-      version: false,
-    },
-  });
-
-  // Start the webpack dev server
-  webpackDevServer.listen(WEBPACK_DEV_SERVER_PORT);
-
-  // Do "hot-reloading" of express stuff on the server
-  // Throw away cached modules and re-require next time
-  // Ensure there's no important state in there!
-  const watcher = chokidar.watch(path.join(cwd, './server'));
-
-  watcher.on('ready', () => {
-    watcher.on('all', (eventName, filePath) => {
-      Object.keys(require.cache).forEach((id) => {
-        if (/\/server\//.test(id)) {
-          delete require.cache[id];
-        }
-      });
-
-      const shortPath = filePath.replace(cwd, '');
-      console.log(`\nModule cache cleared due to change in:\n${shortPath}\n`);
-    });
-  });
+  // Initialize development server
+  devServer(app);
 // Non-development environment configuration
 } else {
   // If in production mode, and the index page is a static path, send the static version
