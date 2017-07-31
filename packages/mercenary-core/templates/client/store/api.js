5d3c111fc87e1c40667ce05adc5d8355cac7bffc
Justin Sisley <sisley.justin@gmail.com>
2017-07-31 12:08:53 -0700

    v2.76.11

--- packages/mercenary-core/templates/client/store/api.js
+++ packages/mercenary-core/templates/client/store/api.js
@@ -6,6 +6,11 @@ import logout from '../utils/logout';
 export default {
   // Handle API errors application-wide
   handleError(dispatch, error) {
+    if (!error.response) {
+      console.log(error.message); // eslint-disable-line
+      return;
+    }
+
     switch (error.response.status) {
       case 401:
         logout();
