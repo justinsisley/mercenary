0d2d9d1713902fb8c28aae68e39b97b5c12bc936
Justin Sisley <jsisley@paypal.com>
2018-03-06 16:32:20 -0800

    v3.20.3

--- packages/core/config/webpack/development.js
+++ packages/core/config/webpack/development.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 const webpack = require('webpack');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const WebappWebpackPlugin = require('webapp-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const eslintFormatter = require('eslint/lib/formatters/stylish');
 const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
@@ -82,7 +82,7 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin(),
 
     // Generate and inject favicon
-    new FaviconsWebpackPlugin(shared.faviconsWebpackPlugin),
+    new WebappWebpackPlugin(shared.faviconsWebpackPlugin),
 
     // Inject generated assets into HTML file
     new HtmlWebpackPlugin({
